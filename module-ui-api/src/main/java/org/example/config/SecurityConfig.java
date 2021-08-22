6f104822d83b3f877843b6e6bea1a847b696327f
Ilyas.Nurymbetov <E789%Wsx65@NNm8PtRR62234>
2021-08-22 22:40:28 +0600

    add style for html page.

--- module-ui-api/src/main/java/org/example/config/SecurityConfig.java
+++ module-ui-api/src/main/java/org/example/config/SecurityConfig.java
@@ -1,17 +1,12 @@
 package org.example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import javax.sql.DataSource;
 
@@ -44,15 +39,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .usersByUsernameQuery("select username, password, is_active from users where username=?")
                 .authoritiesByUsernameQuery("select un.username, ur.user_role from users un inner join user_role ur on un.id = ur.user_id where un.username=?");
     }
-
-//    @Bean
-//    @Override
-//    public UserDetailsService userDetailsService() {
-//        UserDetails userDetails = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("user")
-//                .roles("USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(userDetails);
-//    }
 }
